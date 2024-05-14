6b2fda32cf0b4ccfb3808b066a9e1d3e73a3360b
Ganicse <168539776+Ganicse@users.noreply.github.com>
2024-05-14 15:56:29 +0530

    Add files via upload

---
+++ LeapYear.java
@@ -0,0 +1,8 @@
+public class LeapYear{
+public static void main(String args[]){
+int year=2020;
+If(year % 4 == 0 && year % 100, ! 100 !=0)// year400 == 0;
+Else
+system;out.println(year + " is not a leap year.");
+}
+}
